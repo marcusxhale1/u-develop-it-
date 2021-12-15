97e750389b82a428303ed9efd1c07c1b369dc5f3
Marcus Hale <marcusxhale1@gmail.com>
2021-12-15 01:20:51 -0800

    server is connect

---
+++ server.js
@@ -0,0 +1,25 @@
+const { APPCENTER } = require('ci-info');
+const express = require('express');
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+// Express middleware 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// the code below helps to see if Express server is connected
+
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World'
+//     });
+// });
+
+//Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
