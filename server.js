0eb659580cbf5aba9f2511c96b29be603afab750
Marcus Hale <marcusxhale1@gmail.com>
2021-12-16 01:17:27 -0800

    alright

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const mysql = require('mysql2');
 const { APPCENTER } = require('ci-info');
 const express = require('express');
 const { rosybrown } = require('color-name');
+const { appendToMemberExpression } = require('@babel/types');
+const inputCheck = require('./inputCheck');
 const PORT = process.env.PORT || 3002;
 const app = express();
 
@@ -22,24 +24,53 @@ const db = mysql.createConnection(
     console.log('Connected to the election database!')
 );
 //GET ALL CANDIDATES ON QUERY 
-app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
+// app.get('/api/candidate/:id', (req, res) => {
+//     const sql = `SELECT * FROM candidates WHERE id = ?`;
+//     const params = [req.params.id];
   
-    db.query(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: row
-      });
+//     db.query(sql, params, (err, row) => {
+//       if (err) {
+//         res.status(400).json({ error: err.message });
+//         return;
+//       }
+//       res.json({
+//         message: 'success',
+//         data: row
+//       });
+//     });
+//   });
+
+
+// CREATE THE CANDIDATE 
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+
+    
+    
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error : err.message });
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
     });
   });
 
 
 
+
+
+  
+  
 
 
 //GET SINGLE CANDIDATES 
@@ -54,26 +85,28 @@ app.get('/api/candidate/:id', (req, res) => {
 
 //DELETE CANDIDATES 
 
-app.delete('/api/candidate/:id', (req, res) => {
-    const sql = `DELETE FROM candidates WHERE id = ?`;
-    const params = [req.params.id];
+// app.delete('/api/candidate/:id', (req, res) => {
+//     const sql = `DELETE FROM candidates WHERE id = ?`;
+//     const params = [req.params.id];
   
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.statusMessage(400).json({ error: res.message });
-      } else if (!result.affectedRows) {
-        res.json({
-          message: 'Candidate not found'
-        });
-      } else {
-        res.json({
-          message: 'deleted',
-          changes: result.affectedRows,
-          id: req.params.id
-        });
-      }
-    });
-  });
+//     db.query(sql, params, (err, result) => {
+//       if (err) {
+//         res.statusMessage(400).json({ error: res.message });
+//       } else if (!result.affectedRows) {
+//         res.json({
+//           message: 'Candidate not found'
+//         });
+//       } else {
+//         res.json({
+//           message: 'deleted',
+//           changes: result.affectedRows,
+//           id: req.params.id
+//         });
+//       }
+//     });
+//   });
+
+
 
 //CREATE A CANDIDATE 
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
