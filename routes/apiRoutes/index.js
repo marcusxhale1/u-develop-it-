d59cea378e2e7b432405d8a9b9dc1d8026130a9b
Marcus Hale <marcusxhale1@gmail.com>
2021-12-20 23:36:10 -0800

    done with MySql

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,6 @@ const router = express.Router();
 router.use(require('../../routes/apiRoutes/candidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
+router.use(require('./voteRoutes'));
 
 module.exports = router; 
